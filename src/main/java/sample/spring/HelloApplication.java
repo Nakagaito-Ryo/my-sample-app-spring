58528d373225d1d072fb7f703df87a6cdc61009c
ryo-nakagaito <ryo-nakagaito@mamezou.com>
2024-05-17 16:40:42 +0900

    sample_commit

--- src/main/java/sample/spring/HelloApplication.java
+++ src/main/java/sample/spring/HelloApplication.java
@@ -1,5 +1,10 @@
 package sample.spring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -14,18 +19,42 @@ public class HelloApplication {
     public static void main(String[] args) {
         String empty = new String();
 
-        Cookie c = new Cookie("COOKIENAME", "sensitivedata");
+        Cookie c = new Cookie("COOKIENAME",
+                "sensitivedata");
         c.setSecure(false);
 
-        SpringApplication.run(HelloApplication.class, args);
+        List<String> Bbb = new ArrayList<>();
+        Bbb.add("empty");
+
+        boolean flg = true;
+
+        if (flg) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8,
+                9, 10, 11, 12, 13, 14, 15, 16, 17,
+                18, 19, 20, 21, 23, 24, 25, 26,
+                27, 28, 29, 30};
+        Arrays.asList(numbers).stream().filter(
+                numberrrrrrrrrr -> numberrrrrrrrrr.length == 2)
+                .collect(Collectors.toList());
+
+        SpringApplication.run(
+                HelloApplication.class, args);
     }
 
     @Bean
     TomcatServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        TomcatServletWebServerFactory tomcatServletWebServerFactory =
+                new TomcatServletWebServerFactory();
         LogbackValve valve = new LogbackValve();
-        valve.setFilename(LogbackValve.DEFAULT_FILENAME);
-        tomcatServletWebServerFactory.addContextValves(valve);
+        valve.setFilename(
+                LogbackValve.DEFAULT_FILENAME);
+        tomcatServletWebServerFactory
+                .addContextValves(valve);
         return tomcatServletWebServerFactory;
     }
 }
---
+++ src/main/java/sample/spring/SampleClass.java
@@ -0,0 +1,8 @@
+package sample.spring;
+
+public class SampleClass {
+
+    public int number;
+    private String text;
+
+}
